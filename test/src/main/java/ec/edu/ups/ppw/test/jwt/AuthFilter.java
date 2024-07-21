87237491896d1f1eeaa7458292d4893b9fe481e3
rober <rober@B550mRYZEN>
2024-07-21 11:07:47 -0500

    Implementacion de filtros

--- test/src/main/java/ec/edu/ups/ppw/test/jwt/AuthFilter.java
+++ test/src/main/java/ec/edu/ups/ppw/test/jwt/AuthFilter.java
@@ -8,23 +8,31 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.SecurityContext;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
 @Priority(Priorities.AUTHENTICATION)
 public class AuthFilter implements ContainerRequestFilter, ContainerResponseFilter{
-
+	
+	// Para resolver los problemas de CORS
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
 			throws IOException {
 		// TODO Auto-generated method stub
-		
+		responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+		responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", 
+	            						"GET, POST, PUT, DELETE, OPTIONS");
+		responseContext.getHeaders().putSingle("Access-Control-Allow-Headers",
+		           			"content-type, Authorization");
 	}
-
+	
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub)
+		SecurityContext context = requestContext.getSecurityContext();
+		String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 	}
 
 }
